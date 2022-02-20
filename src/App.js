43047a5470231f46a21e2b0bd867636bf21457a8
Felipe Slaughter-Quintero <fslauq@gmail.com>
2022-02-20 18:20:36 -0500

    added current date and time with css styling, set country state to set selected country in state.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
 
   useEffect(() => {
     const fetchCountriesData = async () => {
@@ -20,18 +21,35 @@ function App() {
     fetchCountriesData();
   }, []);
 
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value;
+    setCountry(countryCode);
+  };
+
+  const currentDate = new Date();
+  const date = currentDate.toLocaleDateString();
+  const time = currentDate.toLocaleTimeString();
+
   return (
     <div className="app">
       <div className="app__header">
-        <h1>COVID-19 Tracker | 2022 </h1>
+        <h1>COVID-19 Tracker</h1>
         <FormControl className="app_dropdown">
-          <Select variant="outlined" value="abc">
-            {/* Loop through countries and show in a dropdown */}
-            {countries.map((country) => (
-              <MenuItem value={country.value}> {country.name} </MenuItem>
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country, i) => (
+              <MenuItem key={i} value={country.value}>
+                {" "}
+                {country.name}{" "}
+              </MenuItem>
             ))}
           </Select>
         </FormControl>
+      </div>
+      <div className="app__datetime">
+        <h5>
+          {date} {time}
+        </h5>
       </div>
 
       {/* Header */}
