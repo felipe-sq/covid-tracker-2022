d1c35c4886ce5a2beea2f5fc4ca38d069dd18d68
Felipe Slaughter-Quintero <fslauq@gmail.com>
2022-02-20 18:05:22 -0500

    installed additional mui npm packages, wrote state and useEffect functions for countries data, added map function for countries dropdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { FormControl, MenuItem, Select } from "@mui/material";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    fetchCountriesData();
+  }, []);
+
   return (
     <div className="app">
-      <h1>COVID 19 Tracker: 2022 </h1>
+      <div className="app__header">
+        <h1>COVID-19 Tracker | 2022 </h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop through countries and show in a dropdown */}
+            {countries.map((country) => (
+              <MenuItem value={country.value}> {country.name} </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
 
       {/* Header */}
       {/* Title + Select Input Dropdown */}
